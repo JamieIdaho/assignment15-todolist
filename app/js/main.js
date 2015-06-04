69561468ff13d4e92e845c7198aa3a5bb7bf6a29
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-06-04 15:36:02 -0400

    got the to do items added with the open and closed status toggling upon click

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,65 @@
-console.log('This is cool!');
+//Todo blueprint
+var Todo = function (options) {
+  var args = options || {};
+  this.task = args.task;
+  this.status = 'Open';
+};
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+//task list
+var storageBin= [];
+
+
+// set up submit
+// on submit create new instance
+$('#addTask').on('submit', function(event)  {
+
+  event.preventDefault();
+  var taskText = $('#taskText').val();
+  var taskInstance = new Todo({ task: taskText });
+
+// store for later
+  storageBin.push(taskInstance);
+
+  $('#tasks').append('<li>' + taskText + '</li>');
+
+  //reset form
+  this.reset();
+
+});
+
+//toggle item
+
+$('#tasks').on('click', 'li', function (event) {
+
+  event.preventDefault();
+  //grab list item clicked on
+  //mark item as completed
+  $(this).addClass('complete');
+
+  var tTask = $(this).text();
+  var taskToEdit = _.find(storageBin, {task: tTask});
+
+  taskToEdit.status = 'Closed';
+
+  });
+
+$('#tasks').on('click', '.complete', function (event) {
+
+   event.preventDefault();
+   $(this).removeClass('complete');
+   var tTask = $(this).text();
+   var taskToEdit = _.find(storageBin, {task: tTask});
+   taskToEdit.status = 'Open';
+
+
+});
+
+
+
+
+
+
+
+
+
+
